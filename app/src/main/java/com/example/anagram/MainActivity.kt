a1b254fdd5d10a7b960e2fcda32ef3c593a8a0f2
unknown <serhiiromanchuk@gmail.com>
2023-04-27 14:18:29 +0300

    Create activity_main.xml

--- app/src/main/java/com/example/anagram/MainActivity.kt
+++ app/src/main/java/com/example/anagram/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.anagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.apply {
+            displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+            setCustomView(R.layout.custom_action_bar)
+            setBackgroundDrawable(
+                ContextCompat.getDrawable(
+                    this@MainActivity,
+                    R.drawable.action_bar_background
+                )
+            )
+        }
     }
 }
