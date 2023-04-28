2cd8eed5b17c2f9a4e725791de3a866666cae80e
unknown <serhiiromanchuk@gmail.com>
2023-04-28 18:04:12 +0300

    Add landscape orientation

--- app/src/main/java/com/example/anagram/MainActivity.kt
+++ app/src/main/java/com/example/anagram/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.anagram
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.View
 import androidx.appcompat.app.ActionBar
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.anagram.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,9 +26,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        setCustomActionBar()
+        checkOrientation()
         setEditTextListeners()
         observeViewModel()
+    }
+
+    private fun checkOrientation() {
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            setCustomActionBar()
+        } else {
+            supportActionBar?.hide()
+        }
     }
 
     private fun setCustomActionBar() {
@@ -47,13 +61,18 @@ class MainActivity : AppCompatActivity() {
                     start: Int,
                     count: Int,
                     after: Int
-                ) { }
+                ) {
+
+                }
 
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    setTextParams(etEnterText)
                     getAnagram()
                 }
 
-                override fun afterTextChanged(s: Editable?) { }
+                override fun afterTextChanged(s: Editable?) {
+
+                }
             })
 
             etFilter.addTextChangedListener(object : TextWatcher {
@@ -73,6 +92,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setTextParams(editText: TextInputEditText) {
+        if (editText.text.toString().isNotBlank()) {
+            editText.gravity = Gravity.START
+            editText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
+            editText.textAlignment = View.TEXT_ALIGNMENT_TEXT_START
+        } else {
+            editText.gravity = Gravity.CENTER
+            editText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14f)
+            editText.textAlignment = View.TEXT_ALIGNMENT_CENTER
+        }
+    }
+
     private fun getAnagram() {
         with(binding) {
             val text = etEnterText.text.toString().trim()
@@ -87,9 +118,27 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun observeViewModel(){
+    private fun observeViewModel() {
         viewModel.anagram.observe(this) {
-            binding.tvAnagram.text = it
+            with(binding) {
+                if (it.isEmpty()) {
+                    tvAnagram.text = getString(R.string.here_must_be_your_anagram)
+                    tvAnagram.setTextColor(
+                        ContextCompat.getColor(
+                            this@MainActivity,
+                            R.color.gray
+                        )
+                    )
+                } else {
+                    tvAnagram.text = it
+                    tvAnagram.setTextColor(
+                        ContextCompat.getColor(
+                            this@MainActivity,
+                            R.color.yellow
+                        )
+                    )
+                }
+            }
         }
     }
 }
